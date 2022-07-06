6d5eae83937cefe32b1f257dbe3ff14efe837a07
kelsonbatista <kelsonbatista@gmail.com>
2022-07-06 18:59:02 -0300

    feat: added teams create migration file

---
+++ app/backend/src/database/migrations/02-teams-create.js
@@ -0,0 +1,17 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('teams', {
+      id: {
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+      },
+      team_name: {
+        type: Sequelize.STRING,
+      }
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('teams');
+  },
+};
