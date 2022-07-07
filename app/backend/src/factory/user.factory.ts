486790597ea07bb27062fddd5a100bc7ad8b3941
kelsonbatista <kelsonbatista@gmail.com>
2022-07-07 11:54:16 -0300

    fix(user-factory): removed db cofig from model

--- app/backend/src/factory/user.factory.ts
+++ app/backend/src/factory/user.factory.ts
@@ -1,11 +1,10 @@
 import UserController from '../controllers/user.controller';
 import UserRepository from '../repository/user.repository';
 import UserService from '../services/user.service';
-import config = require('../database/config/database');
 
 export default class UserFactory {
   public controller = () => {
-    const model = new UserRepository(config);
+    const model = new UserRepository();
     const service = new UserService(model);
     const controller = new UserController(service);
     return controller;
