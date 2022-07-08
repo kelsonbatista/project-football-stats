c0707de15a55d4f671bbb421f2a5f5c3c4f368f5
kelsonbatista <kelsonbatista@gmail.com>
2022-07-07 22:56:04 -0300

    feat(login): added validation on login route

--- app/backend/src/routes/login.router.ts
+++ app/backend/src/routes/login.router.ts
@@ -1,10 +1,11 @@
 import * as express from 'express';
 import UserFactory from '../factory/user.factory';
+import validateLogin from '../middlewares/validations/login.validate.middleware';
 
 const loginRouter = express.Router();
 const userFactory = new UserFactory();
 const userController = userFactory.controller();
 
-loginRouter.post('/', userController.login);
+loginRouter.post('/', validateLogin, userController.login);
 
 export default loginRouter;
