24eb02021d9f1de70f7c0f48507797ff56f07714
kelsonbatista <kelsonbatista@gmail.com>
2022-07-08 23:02:36 -0300

    refactor(validateLogin): changed return of validadeLogin middleware

--- app/backend/src/middlewares/validations/login.validate.middleware.ts
+++ app/backend/src/middlewares/validations/login.validate.middleware.ts
@@ -9,10 +9,13 @@ const validateLogin = async (req: Request, res: Response, next: NextFunction) =>
     const details = error.details[0];
     if (details.type === 'any.required') {
       return res.status(StatusCodes.BAD_REQUEST).json({ message: details.message });
+      // return res.status(StatusCodes.BAD_REQUEST).json({ message: 'All fields must be filled' });
     }
     return next({
-      status: StatusCodes.UNPROCESSABLE_ENTITY,
-      message: details.message,
+      // status: StatusCodes.UNPROCESSABLE_ENTITY,
+      // message: details.message,
+      status: StatusCodes.BAD_REQUEST,
+      message: 'All fields must be filled',
     });
   }
   next();
