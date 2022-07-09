98f911e403befd9c88dcf5a0789b36dc92fed290
kelsonbatista <kelsonbatista@gmail.com>
2022-07-08 22:59:09 -0300

    refactor(auth-middleware): auth token middleware modified

--- app/backend/src/middlewares/auth.token.middleware.ts
+++ app/backend/src/middlewares/auth.token.middleware.ts
@@ -2,11 +2,8 @@ import 'dotenv/config';
 import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import * as jwt from 'jsonwebtoken';
-import UserFactory from '../factory/user.factory';
 
-const secret: any = process.env.SECRET_KEY;
-const userFactory = new UserFactory();
-const userService = userFactory.service();
+const secret: any = process.env.JWT_SECRET;
 
 declare module 'jsonwebtoken' {
   export interface UserIDJwtPayload extends jwt.JwtPayload {
@@ -14,22 +11,14 @@ declare module 'jsonwebtoken' {
   }
 }
 
-// verifica se o token existe no header
-// verifica se o token é valido
-// verifica se o usuario do token informado existe no banco de dados
-// se o usuario nao existe, retorna erro, caso contrario segue o middleware
-const authToken = async (req: Request, res: Response, next: NextFunction) => {
+const authToken = async (req: Request, _res: Response, next: NextFunction) => {
   try {
     const token = req.headers.authorization;
     if (!token) {
       return next({ status: StatusCodes.UNAUTHORIZED, message: 'Token not found' });
     }
     const decode = <jwt.UserIDJwtPayload>jwt.verify(token, secret);
-    const user = await userService.getUserById(decode.data.id);
-    req.user = user;
-    if (!user) {
-      return next({ status: StatusCodes.BAD_REQUEST, message: 'User not found' });
-    }
+    req.query.user = decode.data;
     next();
   } catch (err) {
     console.log(`Error: ${err}`);
