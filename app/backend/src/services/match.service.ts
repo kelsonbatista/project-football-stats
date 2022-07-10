b0787384d239c5d79af962c6abf5f5f2e2d188aa
kelsonbatista <kelsonbatista@gmail.com>
2022-07-10 14:07:38 -0300

    refactor(matches): changed order of return in create match service

--- app/backend/src/services/match.service.ts
+++ app/backend/src/services/match.service.ts
@@ -25,16 +25,16 @@ export default class MatchService implements IMatchService {
 
   public async createMatch(match: IMatch): Promise<IMatch> {
     const { homeTeam, awayTeam } = match;
-    if (homeTeam === awayTeam) {
-      const error = new Error('It is not possible to create a match with two equal teams');
-      error.name = 'UNAUTHORIZED';
-      throw error;
-    }
     const getHomeTeam = homeTeam && await this.teamService.getTeamById(homeTeam.toString());
     const getAwayTeam = awayTeam && await this.teamService.getTeamById(awayTeam.toString());
     if (!getHomeTeam || !getAwayTeam) {
       const error = new Error('There is no team with such id!');
       error.name = 'NOT_FOUND';
+      throw error;
+    }
+    if (homeTeam === awayTeam) {
+      const error = new Error('It is not possible to create a match with two equal teams');
+      error.name = 'UNAUTHORIZED';
       throw error;
     }
     const result = await this.model.createMatch(match);
